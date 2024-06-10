32f61c10ccb37ede0193fc3a6f6e69fcdad22cf4
Fernando Moreira <fernando@parteincerta.com>
2024-06-10 20:31:44 +0100

    Replace `eval` with `envsubst` in hosts file setup

--- shared/scripts/install-hosts.sh
+++ shared/scripts/install-hosts.sh
@@ -6,9 +6,8 @@ this_script_rel_path="$(dirname ${BASH_SOURCE[0]})"
 set -e
 source "$this_script_rel_path/helper.sh"
 trap trap_error ERR
-trap "rm -rf $TMPDIR/hosts" EXIT
+trap "rm -rf $TMPDIR/{hosts,hosts-additions}" EXIT
 
-hostname="${1:-${HOSTNAME%.*}}"
 version="3.14.76"
 url="https://raw.githubusercontent.com/StevenBlack/hosts/${version}/hosts"
 system="$(uname -s)"
@@ -23,25 +22,28 @@ if [ "$system" = "Darwin" ]; then
 	echo "-> Applying additions ..."
 	# ------------------------------
 
+	hosts_additions_orig="$this_script_rel_path/../install-hosts-additions.json"
+	hosts_additions_subst="$TMPDIR/hosts-additions"
+	export hostname="${1:-${HOSTNAME%.*}}"
+	envsubst <"$hosts_additions_orig" >"$hosts_additions_subst"
+
 	echo -e "\n# START --- General additions" >>"$TMPDIR/hosts"
-	shared_address_list=$(jq -r ".shared | keys[]" "$this_script_rel_path/../install-hosts-additions.json")
+	shared_address_list=$(jq -r ".shared | keys[]" "$hosts_additions_subst")
 	for addr in ${shared_address_list[*]}; do
-		shared_names_list=$(jq -r ".shared[\"$addr\"][]" "$this_script_rel_path/../install-hosts-additions.json")
+		shared_names_list=$(jq -r ".shared[\"$addr\"][]" "$hosts_additions_subst")
 		for name in ${shared_names_list[*]}; do
-			entry=$(eval echo "$addr $name")
-			echo -e "$entry" >>"$TMPDIR/hosts"
+			echo -e "$addr $name" >>"$TMPDIR/hosts"
 		done
 	done
 	echo -e "# END --- General additions" >>"$TMPDIR/hosts"
 
 	if [ -n "$1" ]; then
 		echo -e "\n# START --- Specific additions for $1" >>"$TMPDIR/hosts"
-		specific_address_list=$(jq -r ".specific.$1 // {} | keys[]" "$this_script_rel_path/../install-hosts-additions.json")
+		specific_address_list=$(jq -r ".specific.$1 // {} | keys[]" "$hosts_additions_subst")
 		for addr in ${specific_address_list[*]}; do
-			specific_names_list=$(jq -r ".specific.$1[\"$addr\"][]" "$this_script_rel_path/../install-hosts-additions.json")
+			specific_names_list=$(jq -r ".specific.$1[\"$addr\"][]" "$hosts_additions_subs")
 			for name in ${specific_names_list[*]}; do
-				entry=$(eval echo "$addr $name")
-				echo -e "$entry" >>"$TMPDIR/hosts"
+				echo -e "$addr $name" >>"$TMPDIR/hosts"
 			done
 		done
 		echo -e "# END --- Specific additions for $1" >>"$TMPDIR/hosts"
