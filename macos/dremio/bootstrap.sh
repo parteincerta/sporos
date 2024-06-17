8c515f4b8ebf1d55c4309e440edfb6fb84b7c0da
Fernando Moreira <fernando@parteincerta.com>
2024-06-17 15:58:35 +0100

    Add `tailscale` cask to `dremio`

--- macos/dremio/bootstrap.sh
+++ macos/dremio/bootstrap.sh
@@ -68,7 +68,8 @@ microsoft=(microsoft-{excel,powerpoint,remote-desktop,word})
 homebrew_casks=(
 	alt-tab araxis-merge basictex betterdisplay brave-browser bruno chatgpt
 	$compass docker fork iina kitty mac-mouse-fix ${microsoft[*]} numi obs
-	signal slack tableplus transmission visual-studio-code whatsapp zoom
+	signal slack tableplus tailscale transmission visual-studio-code whatsapp
+	zoom
 )
 brew install --cask ${homebrew_casks[*]}
 
