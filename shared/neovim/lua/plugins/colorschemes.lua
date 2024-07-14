23f04c61c1deb97874392a411c60dec1d0c92b5f
Fernando Moreira <fernando@parteincerta.com>
2024-07-14 19:21:20 +0100

    Update Lazy and TokyoNight neovim plugins

--- shared/neovim/lua/plugins/colorschemes.lua
+++ shared/neovim/lua/plugins/colorschemes.lua
@@ -1,52 +1,52 @@
 -- Colorscheme
--- https://folke/tokyonight.nvim
+-- https://github.comfolke/tokyonight.nvim
 
 return {
-	"folke/tokyonight.nvim",
-	enabled = true,
-	version = "2.x",
-	config = function ()
+    "folke/tokyonight.nvim",
+    enabled = true,
+    version = "4.x",
+    lazy = false,
+    priority = 1000,
+    config = function ()
         if os.getenv("COLORTERM") ~= "truecolor" then
             vim.cmd[[set notermguicolors]]
             vim.cmd[[colorscheme default]]
             return
         end
 
-		local tokyo = require("tokyonight")
-		tokyo.setup({
-			style = 'moon',
-			transparent = true,
-			on_highlights = function (hl, c)
-				hl.BufferLineBackground = { fg = c.comment, bg = c.bg }
-				hl.BufferLineBuffer = { fg = c.comment, bg = c.bg }
-				hl.BufferLineBufferSelected = { fg = c.fg, bg = c.bg, bold = true }
-				hl.BufferLineBufferVisible = { fg = c.fg, bg = c.bg }
-				hl.BufferLineCloseButton = { fg = c.comment, bg = c.bg }
-				hl.BufferLineCloseButtonSelected = { fg = c.fg, bg = c.bg, bold = true }
-				hl.BufferLineCloseButtonVisible = { fg = c.fg, bg = c.bg }
-				hl.BufferLineTab = { fg = c.comment, bg = c.bg }
-				hl.BufferLineTabSelected = { fg = c.fg, bg = c.bg, bold = true }
-				hl.BufferLineTabSeparator = { fg = c.bg, bg = c.bg }
-				hl.BufferLineTabSeparatorSelected = { fg = c.bg, bg = c.bg }
+        local tokyo = require("tokyonight")
+        tokyo.setup({
+            style = 'moon',
+            transparent = true,
+            on_highlights = function (hl, c)
+                -- hl.BufferLineBackground = { fg = c.comment, bg = c.bg }
+                -- hl.BufferLineBuffer = { fg = c.comment, bg = c.bg }
+                -- hl.BufferLineBufferSelected = { fg = c.fg, bg = c.bg, bold = true }
+                -- hl.BufferLineBufferVisible = { fg = c.fg, bg = c.bg }
+                -- hl.BufferLineCloseButton = { fg = c.comment, bg = c.bg }
+                -- hl.BufferLineCloseButtonSelected = { fg = c.fg, bg = c.bg, bold = true }
+                -- hl.BufferLineCloseButtonVisible = { fg = c.fg, bg = c.bg }
+                -- hl.BufferLineTab = { fg = c.comment, bg = c.bg }
+                -- hl.BufferLineTabSelected = { fg = c.fg, bg = c.bg, bold = true }
+                -- hl.BufferLineTabSeparator = { fg = c.bg, bg = c.bg }
+                -- hl.BufferLineTabSeparatorSelected = { fg = c.bg, bg = c.bg }
 
-				hl.ColorColumn = { bg = c.bg_highlight }
+                -- hl.NeoTreeFloatBorder = { bg = c.bg }
+                -- hl.NeoTreeFloatTitle = { bg = c.bg }
+                -- hl.NeoTreeNormal = { bg = c.bg }
+                -- hl.NeoTreeNormalNC = { bg = c.bg }
 
-				hl.NeoTreeFloatBorder = { bg = c.bg }
-				hl.NeoTreeFloatTitle = { bg = c.bg }
-				hl.NeoTreeNormal = { bg = c.bg }
-				hl.NeoTreeNormalNC = { bg = c.bg }
+                hl.ColorColumn = { bg = c.bg_highlight }
+                hl.WinSeparator = { fg = c.dark3 }
 
-				hl.WinSeparator = { fg = c.dark3 }
-
-				hl['@text.diff.add'] = { link = "diffAdded" }
-				hl['@text.diff.delete'] = { link = "diffRemoved" }
-				hl['DiagnosticUnnecessary'] = { link = "@comment" }
-			end,
-			styles = {
-				comments = { italic = false },
-			},
-
-            vim.cmd[[colorscheme tokyonight-moon]]
-		})
-	end,
+                hl['@text.diff.add'] = { link = "diffAdded" }
+                hl['@text.diff.delete'] = { link = "diffRemoved" }
+                hl['DiagnosticUnnecessary'] = { link = "@comment" }
+            end,
+            styles = {
+                comments = { italic = false },
+            },
+        })
+        vim.cmd[[colorscheme tokyonight-moon]]
+    end,
 }
--- shared/neovim/lua/plugins/indentlines.lua
+++ shared/neovim/lua/plugins/indentlines.lua
@@ -2,53 +2,54 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	version = "3.*",
-	config = function ()
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = true,
+    main = "ibl",
+    version = "3.*",
+    config = function ()
         if os.getenv("COLORTERM") ~= "truecolor" then
             return
         end
 
-		vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#E06C75" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#E5C07B" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#61AFEF" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#D19A66" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#98C379" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#C678DD" })
-		vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#56B6C2" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg = "#56B6C2" })
 
-		local highlight = {
-			"RainbowDelimiterRed",
-			"RainbowDelimiterYellow",
-			"RainbowDelimiterBlue",
-			"RainbowDelimiterOrange",
-			"RainbowDelimiterGreen",
-			"RainbowDelimiterViolet",
-			"RainbowDelimiterCyan",
-		}
-		vim.g.rainbow_delimiters = { highlight = highlight }
+        local highlight = {
+            "RainbowDelimiterRed",
+            "RainbowDelimiterYellow",
+            "RainbowDelimiterBlue",
+            "RainbowDelimiterOrange",
+            "RainbowDelimiterGreen",
+            "RainbowDelimiterViolet",
+            "RainbowDelimiterCyan",
+        }
+        vim.g.rainbow_delimiters = { highlight = highlight }
 
-		local hooks = require('ibl.hooks')
-		hooks.register(
-			hooks.type.SCOPE_HIGHLIGHT,
-			hooks.builtin.scope_highlight_from_extmark
-		)
+        local hooks = require('ibl.hooks')
+        hooks.register(
+            hooks.type.SCOPE_HIGHLIGHT,
+            hooks.builtin.scope_highlight_from_extmark
+        )
 
-		local ibl = require("ibl");
-		ibl.setup({
-			enabled = true,
-			indent = {
-				char = " ",
-				tab_char = " ",
-			},
-			scope = {
-				enabled = true;
-				char = "│",
-				show_start = false,
-				show_end = false,
-				highlight = highlight,
-			},
-		})
-	end,
+        local ibl = require("ibl");
+        ibl.setup({
+            enabled = true,
+            indent = {
+                char = " ",
+                tab_char = " ",
+            },
+            scope = {
+                enabled = true;
+                char = "│",
+                show_start = false,
+                show_end = false,
+                highlight = highlight,
+            },
+        })
+    end,
 }
