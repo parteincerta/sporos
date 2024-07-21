52c633fe709b5d56b3e604bdcbf7f19a7ba24963
Fernando Moreira <fernando@parteincerta.com>
2024-07-21 13:09:42 +0100

    Update StevenBlack's hosts to v3.14.89

--- shared/scripts/install-hosts.sh
+++ shared/scripts/install-hosts.sh
@@ -8,7 +8,7 @@ source "$this_script_rel_path/helper.sh"
 trap trap_error ERR
 trap "rm -rf $TMPDIR/{hosts,hosts-additions}" EXIT
 
-version="3.14.88"
+version="3.14.89"
 url="https://raw.githubusercontent.com/StevenBlack/hosts/${version}/hosts"
 system="$(uname -s)"
 
