cd50b1d90e42d34eeef227d622d7e6b26855ad38
Fernando Moreira <fernando@parteincerta.com>
2024-07-27 19:18:45 +0100

    Fix lazy.nvim initialization warning

--- shared/neovim/lua/init-lazynvim.lua
+++ shared/neovim/lua/init-lazynvim.lua
@@ -23,41 +23,40 @@ local lazy_path_plugins = vim.fn.stdpath("data") .. "/lazy"
 
 if not vim.loop.fs_stat(lazy_path)
 then
-    print("plugins: Installing lazy.nvim ...")
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazy_path
-    })
-    vim.notify("plugins: lazy.nvim successfully installed.")
+	print("plugins: Installing lazy.nvim ...")
+	vim.fn.system({
+		"git", "clone", "--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazy_path
+	})
+	vim.notify("plugins: lazy.nvim successfully installed.")
 end
 vim.opt.rtp:prepend(lazy_path)
 
 local lazy_opts = {
-    root = lazy_path_plugins,
-    change_detection = { enabled = false, notify = false },
-    checker = { enabled = false },
-    concurrency = 4,
-    defaults = {
-        lazy = false,
-        version = "v11.*",
-    },
-    git = {
-        timeout = 180,
-    },
-    install = { missing = true },
-    performance = {
-        cache = {
-            enabled = true,
-            path = lazy_path_cache,
-        },
-    },
+	root = lazy_path_plugins,
+	change_detection = { enabled = false, notify = false },
+	checker = { enabled = false },
+	concurrency = 4,
+	defaults = {
+		lazy = false,
+		version = "v11.*",
+	},
+	git = { timeout = 180 },
+	install = { missing = true },
+	performance = {
+		cache = {
+			enabled = true,
+			path = lazy_path_cache,
+		},
+	},
+	spec = {{ import = "plugins" }}
 }
 
 local ok, lazy = pcall(require, "lazy")
 if ok then
-    lazy.setup("plugins", lazy_opts)
+	lazy.setup(lazy_opts)
 else
-    vim.notify("plugins: Failed to require lazy.nvim")
+	vim.notify("plugins: Failed to require lazy.nvim")
 end
